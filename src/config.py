e74adb3ad0a4fae71552a4d85f263003a9055c32
Adavideo <adavideo@gmail.com>
2026-04-07 20:24:19 +0200

    Updated the config to use Ollama cloud. The model now is only configured in config.

--- src/config.py
+++ src/config.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 database_file = "data/sample_database.db"
-ollama_url = os.getenv("OLLAMA_URL", "http://localhost:11434")
+ollama_url = os.getenv("OLLAMA_URL", "https://ollama.com")
 ollama_api_key = os.getenv("OLLAMA_API_KEY", "")
-ollama_model = os.getenv("OLLAMA_MODEL", "gemma4:26b")
+ollama_model = "gemma4:31b-cloud"
